4c3fdf37226ea2235c726ceb58b4754c519fbeb5
karinassini@gmail.com <karinassini@gmail.com>
2023-07-04 12:59:01 +0000

    add: adding prim and kruskal implementation and results

--- core.py
+++ core.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import requests
 import gzip
 import shutil
+import heapq
 
 
 def plot_graph(graph, figsize=(8, 6), arrows=False):
@@ -83,3 +84,52 @@ def read_gr_graph(file_path):
                 _, source, target, weight = line.split()
                 graph.add_edge(source, target, weight=float(weight))
     return graph
+
+
+def prim(graph, start):
+    minimum_spanning_tree = []  # Cria árvore vazia
+    all_vertices = set(graph.keys())
+    visited_vertices = {start}
+    aux_heap = []
+    total_weight = 0  # Custo total da árvore
+
+    # Adicionas da fila os vizinhos do vertice inicial
+    for neighbor, weight in graph[start].items():
+        heapq.heappush(aux_heap, (weight, start, neighbor))
+
+    while len(aux_heap) > 0 and len(visited_vertices) != len(all_vertices):
+        weight, u, v = heapq.heappop(aux_heap)  # Pega e devolva o menor item da pilha
+
+        # pula se já foi visitado
+        if v in visited_vertices:
+            continue
+
+        minimum_spanning_tree.append((u, v, weight))
+        total_weight += weight
+        visited_vertices.add(v)
+
+        # Adiciona arestas do vértice recém-visitado ao heap
+        for neighbor, weight in graph[v].items():
+            if neighbor not in visited_vertices:
+                heapq.heappush(aux_heap, (weight, v, neighbor))
+
+    return minimum_spanning_tree, total_weight
+
+def kruskal(graph):
+    edges = [(n1, n2, graph[n1][n2]) for n1 in graph.keys() for n2 in graph[n1]]
+    edges_sorted = sorted(edges, key=lambda x: x[2])
+    all_sets = {x: {x} for x in graph.keys()}
+    n_graph = len(graph.keys()) - 1
+    total_cost = 0
+    minimum_spanning_tree = []
+
+    for n1, n2, weight in edges_sorted:
+        if all_sets[n1].isdisjoint(all_sets[n2]):
+            values_in_set = all_sets[n1] | all_sets[n2]
+            all_sets[n1] = values_in_set
+            all_sets[n2] = values_in_set
+            total_cost += weight
+            minimum_spanning_tree.append((n1, n2, weight))
+        if len(minimum_spanning_tree) == n_graph:
+            break
+    return minimum_spanning_tree, total_cost
